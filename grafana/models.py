aea3851ff6b0dfc42d8d9ec825470a0be8077fe9
Alex <alexandr@gavrisco.com>
2015-04-13 20:46:09 +0300

    Added mixins with created and updated fields due to DRY principles.

--- grafana/models.py
+++ grafana/models.py
@@ -1,3 +1,19 @@
-from django.db import models
+from __future__ import unicode_literals
 
-# Create your models here.
+
+class CreatedModelMixin(object):
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __init__(self, *args, **kwargs):
+        super(CreatedModelMixin, self).__init__(*args, **kwargs)
+
+
+class UpdatedModelMixin(object):
+    updated = models.DateTimeField(auto_now=True)
+
+    def __init__(self, *args, **kwargs):
+        super(UpdatedModelMixin, self).__init__(*args, **kwargs)
+
+
+class ChangedModelMixin(CreatedModelMixin, UpdatedModelMixin):
+    pass
